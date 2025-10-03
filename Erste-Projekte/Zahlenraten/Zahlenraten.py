1dfd32cf08e447d8e907d557a837689c2db0a8e2
Marco-Vel <marcovelardita29@gmail.com>
2025-10-03 14:59:52 +0200

    Zahlenraten code geschrieben. Fehler muss noch gelöst werden.

---
+++ Erste-Projekte/Zahlenraten/Zahlenraten.py
@@ -0,0 +1,33 @@
+import random
+
+def Zahlenraten():
+    computer = random.randint(1,100)
+
+    while True:
+        versuche = None
+
+        spieler = input("Ich habe mir eine Zahl zwischen 1 und 100 ausgedacht.\n" \
+        "Rate mal an welche Zahl ich denke:\n")
+
+        if int in spieler:
+            if (spieler == computer):
+                versuche = versuche + 1
+                print("Du hast meine Zahl erraten!")
+                if(versuche == 1):
+                    print(f"Du hast einen Versuch gebraucht!")
+                    break
+                else:
+                    print(f"Du hast {versuche} Versuche gebraucht!")
+                    break
+            elif (spieler >= computer):
+                print("Deine Zahl ist größer als meine...")
+                versuche = versuche + 1
+                return
+            elif (computer >= spieler):
+                print("Deine Zahl ist kleiner als meine...")
+                versuche = versuche + 1
+                return
+        else:
+            print("Gebe eine Zahl an!")
+
+Zahlenraten()
